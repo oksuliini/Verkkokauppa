42ab19cfbe24d0c685ef512b1aab7211d921542f
Elmeri Juuso Antero Kuvaja <s3kuel01@students.osao.fi>
2024-11-28 13:19:10 +0200

    Login processia eteenpäin

--- public/login.php
+++ public/login.php
@@ -1,21 +1,53 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kirjaudu sisään</title>
+    <title>Login</title>
+    <link href="loginmodule.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-    <h1>Kirjaudu sisään</h1>
-    <form action="login_process.php" method="POST">
-        <label for="email">Sähköposti:</label><br>
-        <input type="email" id="email" name="email" required><br><br>
+<center> <h1>Login</h1><center>
 
-        <label for="password">Salasana:</label><br>
-        <input type="password" id="password" name="password" required><br><br>
+    <?php
+    if (isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) > 0) {
+        echo '<ul class="err">';
+        foreach ($_SESSION['ERRMSG_ARR'] as $msg) {
+            echo '<li>', $msg, '</li>';
+        }
+        echo '</ul>';
+        unset($_SESSION['ERRMSG_ARR']);
+    }
 
-        <button type="submit">Kirjaudu</button>
+    if (isset($_SESSION['SUCCESS_MSG'])) {
+        echo '<p class="success">' . $_SESSION['SUCCESS_MSG'] . '</p>';
+        unset($_SESSION['SUCCESS_MSG']);
+    }
+    ?>
+
+    <form id="login-form" name="loginform" method="post" action="login_process.php">
+        <table width="300" border="0" align="center" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>Username</th>
+                <td><input name="username" type="text" class="textfield" id="username" required /></td>
+            </tr>
+            <tr>
+                <th>Password</th>
+                <td><input name="password" type="password" class="textfield" id="password" required /></td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td><input type="submit" name="Submit" value="Login" /></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <b>Don't have an account?</b> <a href="register.php">Register here!</a>
+                </td>
+            </tr>
+        </table>
     </form>
-    <p>Ei tiliä? <a href="register.php">Rekisteröidy</a></p>
 </body>
 </html>
--- public/register_process.php
+++ public/register_process.php
@@ -1,112 +1,85 @@
 <?php
-	//Start session
-	session_start();
-	
-	//Include database connection details
-	require_once('config.php');
-	
-	//Array to store validation errors
-	$errmsg_arr = array();
-	
-	//Validation error flag
-	$errflag = false;
-	
-	//Connect to mysql server
-	$link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);
+// Start session
+session_start();
+
+// Include database connection details
+require_once('config.php');
+
+// Array to store validation errors
+$errmsg_arr = array();
+
+// Validation error flag
+$errflag = false;
+
+// Connect to MySQL server
+$link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);
 if (!$link) {
     die('Failed to connect to server: ' . mysqli_connect_error());
 }
 
-	
-	//Function to sanitize values received from the form. Prevents SQL injection
-	function clean($link,$str) {
-		$str = stripslashes($str);
-        return mysqli_real_escape_string($link,$str);
-	}
-	
-	//Sanitize the POST values
-	$first_name = isset($_POST['first_name']) ? clean($link, $_POST['first_name']) : '';
-	$last_name = isset($_POST['last_name']) ? clean($link,$_POST['last_name']) : '';
-	$email = isset($_POST['email']) ? clean($link,$_POST['email']) : '';
-    $phone = isset($_POST['phone']) ? clean($link,$_POST['phone']) : '';
-    $address = isset($_POST['address']) ? clean($link,$_POST['address']) : '';
-	$username = isset($_POST['username']) ? clean($link,$_POST['username']) : '';
-	$password = isset($_POST['password']) ? clean($link,$_POST['password']) : '';
-	$cpassword = isset($_POST['cpassword']) ? clean($link,$_POST['cpassword']) : '';
-	
-	//Input Validations
-	if($first_name == '') {
-		$errmsg_arr[] = 'First name missing';
-		$errflag = true;
-	}
-	if($last_name == '') {
-		$errmsg_arr[] = 'Last name missing';
-		$errflag = true;
-	}
-	if($email == '') {
-		$errmsg_arr[] = 'Email missing';
-		$errflag = true;
-	}
-    if($phone == '') {
-		$errmsg_arr[] = 'Phonenumber is missing';
-		$errflag = true;
-	}
-    if($address == '') {
-		$errmsg_arr[] = 'Address is missing';
-		$errflag = true;
-	}
-	if($username == '') {
-		$errmsg_arr[] = 'Username missing';
-		$errflag = true;
-	}
-	if($password == '') {
-		$errmsg_arr[] = 'Password missing';
-		$errflag = true;
-	}
-	if($cpassword == '') {
-		$errmsg_arr[] = 'Confirm password missing';
-		$errflag = true;
-	}
-	if( strcmp($password, $cpassword) != 0 ) {
-		$errmsg_arr[] = 'Passwords do not match';
-		$errflag = true;
-	}
-	
-	//Check for duplicate login ID
-	if($username != '') {
-		$qry = "SELECT * FROM users WHERE username='$username'";
-		$result = mysqli_query($link,$qry);
-		if($result) {
-			if(mysqli_num_rows($result) > 0) {
-				$errmsg_arr[] = 'Username already in use';
-				$errflag = true;
-			}
-			@mysqli_free_result($result);
-		}
-		else {
-			die("Query failed" . mysqli_error($link));
-		}
-	}
-	
-	//If there are input validations, redirect back to the registration form
-	if($errflag) {
-		$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
-		session_write_close();
-		header("location: register.php");
-		exit();
-	}
+// Function to sanitize values received from the form
+function clean($link, $str) {
+    $str = stripslashes($str);
+    return mysqli_real_escape_string($link, $str);
+}
 
-	//Create INSERT query
-	$qry = "INSERT INTO users(first_name, last_name, email, phone, address, username, password) VALUES('$first_name','$last_name','$email','$phone','$address','$username','".md5($password)."')";
-	$result = @mysqli_query($link,$qry);
-	
-	//Check whether the query was successful or not
-	if($result) {
-		header("location: register-success.php");
-		exit();
-	}else {
-		die("Query failed" . mysqli_error($link));
-	}
+// Sanitize the POST values
+$first_name = isset($_POST['first_name']) ? clean($link, $_POST['first_name']) : '';
+$last_name = isset($_POST['last_name']) ? clean($link, $_POST['last_name']) : '';
+$email = isset($_POST['email']) ? clean($link, $_POST['email']) : '';
+$phone = isset($_POST['phone']) ? clean($link, $_POST['phone']) : '';
+$address = isset($_POST['address']) ? clean($link, $_POST['address']) : '';
+$username = isset($_POST['username']) ? clean($link, $_POST['username']) : '';
+$password = isset($_POST['password']) ? clean($link, $_POST['password']) : '';
+$cpassword = isset($_POST['cpassword']) ? clean($link, $_POST['cpassword']) : '';
 
-    mysqli_close($link);
-?>
+// Input Validations
+if ($first_name == '') $errmsg_arr[] = 'First name missing';
+if ($last_name == '') $errmsg_arr[] = 'Last name missing';
+if ($email == '') $errmsg_arr[] = 'Email missing';
+if ($phone == '') $errmsg_arr[] = 'Phonenumber is missing';
+if ($address == '') $errmsg_arr[] = 'Address is missing';
+if ($username == '') $errmsg_arr[] = 'Username missing';
+if ($password == '') $errmsg_arr[] = 'Password missing';
+if ($cpassword == '') $errmsg_arr[] = 'Confirm password missing';
+if (strcmp($password, $cpassword) != 0) $errmsg_arr[] = 'Passwords do not match';
+
+// Check for duplicate username
+if ($username != '') {
+    $qry = "SELECT * FROM users WHERE username='$username'";
+    $result = mysqli_query($link, $qry);
+    if ($result) {
+        if (mysqli_num_rows($result) > 0) {
+            $errmsg_arr[] = 'Username already in use';
+            $errflag = true;
+        }
+        mysqli_free_result($result);
+    } else {
+        die("Query failed" . mysqli_error($link));
+    }
+}
+
+// If there are input validations, redirect back to the registration form
+if ($errflag) {
+    $_SESSION['ERRMSG_ARR'] = $errmsg_arr;
+    session_write_close();
+    header("location: register.php");
+    exit();
+}
+
+// Hash the password securely
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+// Create INSERT query
+$qry = "INSERT INTO users (first_name, last_name, email, phone, address, username, password) VALUES ('$first_name', '$last_name', '$email', '$phone', '$address', '$username', '$hashed_password')";
+$result = mysqli_query($link, $qry);
+
+// Check whether the query was successful or not
+if ($result) {
+    header("location: login.php"); // Redirect to success page
+    exit();
+} else {
+    die("Query failed" . mysqli_error($link));
+}
+
+mysqli_close($link);
