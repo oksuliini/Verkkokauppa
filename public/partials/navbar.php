8cc47fc1369a1f52c8ae58bcb7f852aa617da99c
Oskari Juhani Kokkoniemi <s3koos01@students.osao.fi>
2024-12-19 09:08:08 +0200

    index toimimaan ja navbar muutoksia

--- public/partials/navbar.php
+++ public/partials/navbar.php
@@ -1,4 +1,3 @@
-
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg bg-pink navbar-light" style="background-color: #ffccd5; font-family: 'Comic Sans MS', cursive;">
   <!-- Container wrapper -->
@@ -51,10 +50,14 @@
         </li>
       </ul>
 
-      <!-- Search -->
-      <form class="d-flex">
+      <!-- Search and Profile -->
+      <form class="d-flex align-items-center">
         <input type="search" class="form-control" placeholder="Search Hello Kitty" aria-label="Search" style="border-radius: 20px;">
-        <button class="btn btn-white" type="submit"><i class="fas fa-search"></i></button>
+        <button class="btn btn-white ms-2" type="submit"><i class="fas fa-search"></i></button>
+        <!-- Profile Icon -->
+        <a href="content/profile.php" class="btn btn-white ms-2">
+          <i class="fas fa-user-circle" style="font-size: 1.5rem; color: #ff6f91;"></i>
+        </a>
       </form>
     </div>
   </div>
